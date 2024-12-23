75679c951ab57539cc36e590d6391ff47b162c53
amin <aminkhm097@gmail.com>
2024-12-23 21:46:44 +0330

    fix: Adding loops for products to optimize code

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -9,13 +9,30 @@ import s21 from "../images/s21.jpg";
 
 
 class Cards extends Component {
+    constructor() {
+        super();
+        this.state = {
+            phoneData : [
+                {image:iphone10 , name:"Iphone X" , price:"500 $"},
+                {image:iphone11 , name:"Iphone 11" , price:"600 $"},
+                {image:iphone12 , name:"Iphone 12" , price:"800 $"},
+                {image:s21 , name:"S21 Ultra" , price:"790 $"}
+            ]
+        }
+    }
     render() {
+        const {phoneData} = this.state;
         return (
             <div className={styles.container}>
-                <Card image={iphone10} name="Iphone X" price="500 $"/>
-                <Card image={iphone11} name="Iphone 11" price="600 $"/>
-                <Card image={iphone12} name="Iphone 12" price="800 $"/>
-                <Card image={s21} name="S21 Ultra" price="790 $"/>
+
+                {phoneData.map(phone => <Card image={phone.image} name={phone.name} price={phone.price}/>)}
+
+
+
+                {/*<Card image={iphone10} name="Iphone X" price="500 $"/>*/}
+                {/*<Card image={iphone11} name="Iphone 11" price="600 $"/>*/}
+                {/*<Card image={iphone12} name="Iphone 12" price="800 $"/>*/}
+                {/*<Card image={s21} name="S21 Ultra" price="790 $"/>*/}
             </div>
         );
     }
