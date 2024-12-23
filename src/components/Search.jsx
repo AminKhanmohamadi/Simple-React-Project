1336296994dd2909d7e7d8e8aabce463ffd2ea78
amin <aminkhm097@gmail.com>
2024-12-23 20:39:04 +0330

    feat: add search preview input

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -3,11 +3,30 @@ import React, {Component} from 'react';
 import styles from "./Search.module.css";
 
 class Search extends Component {
+    constructor() {
+        super();
+        this.state = {
+            text:"",
+        }
+    }
+
+    changeHandler = event => {
+        this.setState({
+            text: event.target.value,
+        })
+    }
+
+
     render() {
+        const {text} = this.state;
         return (
             <div className={styles.container}>
                 <p>Search What you want</p>
-                <input type="text" placeholder="Search ..."/>
+                <div>
+                    <input type="text" value={text} onChange={this.changeHandler} placeholder="Search ..." />
+                    <br/><br/>
+                    <span>{text}</span>
+                </div>
             </div>
         );
     }
